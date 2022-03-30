778ef3f65036f382ee83cc5fd2e36de078d65403
proku <andrejames1021@gmail.com>
2022-03-30 17:28:12 +0400

    learning C, data types, printf, variables

---
+++ test.c
@@ -0,0 +1,101 @@
+// Hello World
+
+            // #include<stdio.h>
+            // #include <stdlib.h>
+
+            // int main(){
+            //     printf("Hello World");
+            //     return 0;
+            // }
+
+// Drawing a shape
+
+            // #include <stdio.h>
+            // #include <stdlib.h>
+
+
+            // int main()
+            // {
+            //     printf("    /| \n");
+            //     printf("   / | \n");
+            //     printf("  /  | \n");
+            //     printf(" /___| \n");
+            // }
+
+// Variables
+
+            // #include <stdlib.h>
+            // #include <stdio.h>
+
+            // int main()
+            // {
+            //     char character_name[] = "proku"; 
+            //     int character_age = 69;
+            //     int garantisado = 32;
+            //     printf("There once was a man named %s.", character_name);
+            //     printf("he was %i years old.", character_age);
+            //     printf("He really liked the name %s ", character_name);
+            //     printf("%i but did not like being %i and being named as '%s'.", character_age,  garantisado, character_name);
+            //     return 0;
+            // }
+
+
+            // int main()
+            // {
+            //     char student_name[] = "hussein";
+            //     printf("%s is a very stupid person.", student_name);
+            //     return 1;
+            // }
+
+// Data Types 
+
+            // #include <stdlib.h>
+            // #include <stdio.h>
+
+            // int main()
+            // {
+                
+            //     int age = 40;
+            //     float grade_of_student = 40.33131312313213081730217308173981273981237918;
+            //     char name_of_student = 'a';
+            //     char description[] = "pinaka-bobong student sa isang eskwelahan, tanginang yan.";
+
+
+            //     printf("angelo's grade is: %d", age);
+            //     return 0;
+            // }
+
+            // #include <stdio.h>
+            // #include <stdlib.h>
+
+// Printf 
+
+// #include <stdlib.h>
+// #include <stdio.h>
+
+// int main()
+// {
+//     char student_name[] = "_____";
+//     int fave_num = 32;
+//     printf("%s's favorite number is %lld",student_name, fave_num);
+//     return 0;
+// }
+
+// Working With Numbers
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main()
+{
+
+    printf("%f", 5 / 4.67);
+    printf("%d", 5 / 4.67);
+    printf("%i", 5 / 4.67);
+
+
+    return 0;
+}
+
+
+    
